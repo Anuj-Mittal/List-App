5c740d066afce181b88b66d719df1039ba71e326
Anuj-Mittal <44200779+Anuj-Mittal@users.noreply.github.com>
2021-06-11 17:59:31 +0530

    Added Item with large title

--- assets/common/js/itemList.js
+++ assets/common/js/itemList.js
@@ -24,5 +24,11 @@ const itemList = [
       "https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
     title: "interns-performance-report-june-2021.key",
   },
+  {
+    previewImage:
+      "https://images.unsplash.com/photo-1623206837956-07dab21608f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+    title:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to heets ",
+  },
 ];
 export default itemList;
